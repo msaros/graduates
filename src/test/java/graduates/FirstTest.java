76da3fcc1dd3d4e03ba6bd5f6b43c8e690f4fc7b
msaros <msaros@EN62387.endava.net>
2015-07-31 13:22:03 +0300

    initial commit

---
+++ src/test/java/graduates/FirstTest.java
@@ -0,0 +1,15 @@
+package graduates;
+
+import org.junit.Test;
+
+/**
+ * Created by msaros on 7/31/2015.
+ */
+public class FirstTest {
+    @Test
+    public void test()
+    {
+        System.out.println("Hello");
+
+    }
+}
---
+++ src/test/java/graduates/SecondTest.java
@@ -0,0 +1,16 @@
+package graduates;
+
+import org.junit.Test;
+
+/**
+ * Created by msaros on 7/31/2015.
+ */
+public class SecondTest {
+    @Test
+    public void test()
+    {
+        String env = System.getProperty("env");
+        System.out.println(env);
+
+    }
+}
